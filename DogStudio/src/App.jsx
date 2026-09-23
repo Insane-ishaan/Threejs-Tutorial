84c18e4a59837a3ccb574081f1799b5011c13107
Ishaan Sharma <ishaanshama93@gmail.com>
2026-09-23 18:59:29 +0530

    Done with Dog Model

--- DogStudio/src/App.jsx
+++ DogStudio/src/App.jsx
@@ -6,9 +6,17 @@ import Dog from './Components/Dog.jsx'
 function App() {
 
   return (
-    <Canvas>
-      <Dog />
-    </Canvas>
+    <main >
+      <div className="canvas-container">
+        <Canvas style={{ position: "fixed", height: "100vh", width: "100vw", backgroundImage: "url(/background-l.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundAttachment: "fixed" }}>
+          <Dog />
+        </Canvas>
+      </div>
+
+      <section className="section section1"></section>
+      <section className="section section2"></section>
+      <section className="section section3"></section>
+    </main>
   )
 }
 
--- DogStudio/src/Components/Dog.jsx
+++ DogStudio/src/Components/Dog.jsx
@@ -1,12 +1,96 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useAnimations, useGLTF, useTexture } from "@react-three/drei";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
+import { useEffect, useMemo } from "react";
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+
 function Dog() {
+    const model = useGLTF("/model/dog.drc.glb");
+    const { actions } = useAnimations(model.animations, model.scene);
+    const { camera, gl } = useThree();
+    useEffect(() => {
+        actions["Take 001"]?.play();
+    }, [actions]);
+
+    camera.position.z = 1.3
+    gl.toneMapping = THREE.NoToneMapping
+    gl.outputColorSpace = THREE.SRGBColorSpace
+
+    const [normalMap, matColor, branchColor, branchNormal] = useTexture(["/dog_normals.jpg", "/mat_colors/mat-2.png", "/branches_diffuse.jpg", "/branches_normals.jpg"]).map((texture) => {
+        texture.flipY = false
+        return texture
+    })
+
+    matColor.colorSpace = THREE.SRGBColorSpace
+    branchColor.colorSpace = THREE.SRGBColorSpace
+    matColor.anisotropy = gl.capabilities.getMaxAnisotropy();
+    branchColor.anisotropy = gl.capabilities.getMaxAnisotropy();
+
+    const DOGMeshMaterial = useMemo(() => {
+        return new THREE.MeshMatcapMaterial({
+            normalMap: normalMap,
+            matcap: matColor,
+            normalScale: new THREE.Vector2(1.1, 1.1),
+        })
+    }, [normalMap, matColor])
+
+    const BranchMeshMaterial = useMemo(() => {
+        return new THREE.MeshMatcapMaterial({
+            normalMap: branchNormal,
+            matcap: branchColor,
+        })
+    }, [branchColor, branchNormal])
+
+    model.scene.traverse((child) => {
+        if (child.isMesh && child.name.includes("DOG")) {
+            child.material = DOGMeshMaterial
+        } else {
+            child.material = BranchMeshMaterial;
+        }
+    })
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".canvas-container",
+                endTrigger: ".section2",
+                start: "top top",
+                end: "bottom bottom",
+                scrub: true
+            }
+        })
+        tl
+            .to(model.scene.position, {
+                z: "-=1.6",
+                y: "+=0.1"
+            })
+
+            .to(model.scene.rotation, {
+                x: "+=0.3",
+            })
+
+            .to(model.scene.rotation, {
+                y: `-=${Math.PI}`
+            }, "third")
+
+            .to(model.scene.position, {
+                x: "-=1",
+                y: "+=0.3",
+                z: "+=0.8"
+            }, "third")
+
+
+    }, []);
+
     return (
         <>
-            <mesh>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshBasicMaterial color="hotpink" />
-            </mesh>
-            <OrbitControls />
+            <primitive object={model.scene} position={[0.4, -1.6, 0]} rotation={[0, Math.PI / 6, 0]} scale={2.8} />
+            <directionalLight position={[0, 5, 2]} intensity={10} />
+            {/* <OrbitControls /> */}
         </>
     );
 }
